930af21c7b13fda688cecbba5d3d2fd1d675925c
devamahesa <devamahesa99@gmail.com>
2024-12-26 12:01:53 +0700

    ubah halaman show materi dan detail

--- resources/views/livewire/pages/tutor/components/materi-show.blade.php
+++ resources/views/livewire/pages/tutor/components/materi-show.blade.php
@@ -17,43 +17,56 @@
 
     <main>
         <div class="container-fluid px-4">
-            <div class="card">
-
+            <div class="card mb-3">
                 <div class="card-header">
-                    <h5 class="">{{ $content->nama_materi }}</h5>
+                    <h5 class="h5">{{ __('Detail') }}</h5>
                 </div>
 
                 <div class="card-body">
-
                     <table class="mt-3">
                         <tr>
-                            <th>Materi</th>
-                            <th> : {{ $content->nama_materi}}</th>
+                            <th class="pe-3">Domain</th>
+                            <td> : {{ $content->subdomain->domain->keterangan}}</td>
                         </tr>
                         <tr>
-                            <th>Domain</th>
-                            <th> : {{ $content->subdomain->domain->keterangan}}</th>
+                            <th class="pe-3">Subdomain</th>
+                            <td> : {{ $content->subdomain->keterangan}}</td>
                         </tr>
                         <tr>
-                            <th>Subdomain</th>
-                            <th> : {{ $content->subdomain->keterangan}}</th>
+                            <th class="pe-3">Materi</th>
+                            <td> : {{ $content->nama_materi}}</td>
                         </tr>
                         <tr>
-                            <th>Waktu & Tanggal</th>
-                            <th> : {{ $content->created_at->diffForHumans() }}</th>
+                            <th class="pe-3">Submateri</th>
+                            <td> : {{ $content->nama_submateri}}</td>
+                        </tr>
+                        <tr>
+                            <th class="pe-3">Dibuat sejak</th>
+                            <td> : {{ $content->created_at->diffForHumans() }}</td>
+                        </tr>
+                        <tr>
+                            <th class="pe-3">Diperbarui sejak</th>
+                            <td> : {{ $content->updated_at->diffForHumans() }}</td>
                         </tr>
                     </table>
-                    <hr>
+                </div>
+            </div>
 
+            <div class="card">
+
+                <div class="card-header">
+                    <h4 class="h4">{{ $content->nama_materi }}</h4>
+                    <p class="text-gray-600 mb-0">{{ $content->nama_submateri }}</p>
+                </div>
+
+                <div class="card-body">
                     <div style="overflow-wrap: break-word;">
                         {!! $content->deskripsi !!}
                     </div>
-
                 </div>
 
-
                 <div class="card-footer">
-                    <a href="{{route('materi.index')}}" class="btn btn-primary mt-3">Kembali</a>
+                    <a href="{{route('materi.index')}}" class="btn btn-primary">Kembali</a>
                 </div>
 
             </div>

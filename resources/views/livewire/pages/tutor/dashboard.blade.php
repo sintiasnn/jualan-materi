fa63e8c8faf4cca16531e1a7a33939f635bdaebd
devamahesa <devamahesa99@gmail.com>
2025-01-12 10:54:42 +0800

    ubah tampilan halaman utama

--- resources/views/livewire/pages/tutor/dashboard.blade.php
+++ resources/views/livewire/pages/tutor/dashboard.blade.php
@@ -1,21 +1,21 @@
 <x-app-layout>
-    {{-- <x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Tutor Dashboard') }}
         </h2>
-    </x-slot> --}}
+    </x-slot>
 
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <header class="page-header page-header-compact page-header-light bg-transparent mb-4">
+        <div class="container-fluid px-5 py-5">
+            <div class="page-header-content">
+                <div class="row align-items-center justify-content-between pt-3">
+                    <div class="col-auto mb-3">
+                        <h1 class="display-6 text-gray-700">
+                            {{ __("Welcome, " . auth()->user()->name) }}
+                        </h1>
+                    </div>
                 </div>
             </div>
-            
         </div>
-    </div> --}}
-    
-
-        
+    </header>
 </x-app-layout>
